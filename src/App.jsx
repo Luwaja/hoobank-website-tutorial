5bff5e267090b5a3f78e560a7507fa4172eb267e
Luke James <luke.james924@gmail.com>
2024-01-23 08:08:55 -0600

    Simple Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import styles from './style';
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
-      <h1>Hello, World!</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
     </div>
   )
 
